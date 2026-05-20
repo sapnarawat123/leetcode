bcb3b3ba334a7a4f1a2e7dab58870f3084fa6e33
sapnarawat123 <sapnarawat498@gmailcom>
2026-05-20 12:46:01 +0530

    max sum of subArray using kandane algorithm leetcode problem no 53

--- maxsubArraySum.js
+++ maxsubArraySum.js
@@ -26,3 +26,19 @@ var maxSubArray = function(nums){
 }
 
 console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+//USING KANDANE ALGORITHM
+
+var sub = function maxSum(nums){
+    let currentSum=0;
+    let maxSum = -Infinity;
+    for(let i=0;i<nums.length;i++){
+        currentSum += nums[i];
+        maxSum = Math.max(maxSum,currentSum)
+            
+            if(currentSum<0){
+            currentSum = 0; // fresh start — negative sum ko aage carry karna fayda nahi
+            }
+           }
+           return maxSum;
+}
