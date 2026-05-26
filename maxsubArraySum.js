6c2680df6d189cb315841a632ed999a2bf4ca377
sapnarawat123 <sapnarawat498@gmailcom>
2026-05-26 10:11:50 +0530

    move zeroes

--- maxsubArraySum.js
+++ maxsubArraySum.js
@@ -1,11 +1,4 @@
-// 53. Maximum Subarray
-// Given an integer array nums, find the subarray with the largest sum, and return its sum.
-
-// Example 1:
-
-// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-// Output: 6
-// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+// sum of an contiguous subarray with the largest sum
 
 //using brute force approach time complexity is O(n^2) and space complexity is O(1)
 
@@ -54,3 +47,57 @@ var sub= function maxSum(nums){
     }
     return maxSum;
 }
+//////////////////////////////LEET CODE 523. Continuous Subarray Sum
+/*
+Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+its length is at least two, and
+the sum of the elements of the subarray is a multiple of k.
+Note that:
+
+A subarray is a contiguous part of the array.
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ 
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+Example 2:
+
+Input: nums = [23,2,6,4,7], k = 6
+Output: true
+Explanation: [23, 2, 6, 4, 7] is an continuous subarray of size 5 whose elements sum up to 42.
+42 is a multiple of 6 because 42 = 7 * 6 and 7 is an integer.
+Example 3:
+
+Input: nums = [23,2,6,4,7], k = 13
+Output: false*/
+
+var checkSubarraySum = function(nums,k) {
+     let maxSum = 0;
+     let subArray =[];
+     for(let i=0;i<nums.length;i++){
+         let currentSum = 0;
+         for(let j=i;j<nums.length;j++){
+             currentSum +=nums[j]
+             let length =j-i+1
+             if(currentSum % k===0 && length>=2 ){
+         console.log("inside if")
+     return true;
+     }
+    }       
+     }
+
+     
+     return false
+};
+
+// console.log(checkSubarraySum([-2, 1, -3, 4, -1, 2, 1, -5, 4],4))
+
+/*did above solution using brute force approach time complexity is O(n^2) and space complexity is O(1)
+therefore it is not efficient for large input size, we can optimize it using hash map to store the cumulative sum and its index, so that its time limit won't exceed for larger ip
+
+//using hash map to store the cumulative sum and its index time complexity is O(n) and space complexity is O(n)
---
+++ moveZeroes.js
@@ -0,0 +1,30 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+ 
+// SOLUTION
+
+var moveZeroes = function(nums){
+    let j=0;
+    for(i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            console.log(nums[i]);
+            [nums[j],nums[i]]=[nums[i],nums[j]];
+            j++;
+        }
+    }
+    return nums;
+}
+var nums = [0,1,0,3,12]
+console.log(moveZeroes(nums))
